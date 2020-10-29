f5c4ed83f31eccf0e4fd6580a123af56f84fc6ab
xiao <gds1105928109@qq.com>
2020-10-29 20:11:41 +0800

    项目初始化，fragment基本布局已实现

---
+++ app/src/main/java/com/example/fragment/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.fragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.left_fragment.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+       button.setOnClickListener {
+           replaceFragment(AnotherRightFragment())
+       }
+        replaceFragment(RightFragment())
+    }
+
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager =supportFragmentManager
+        val transaction =fragmentManager.beginTransaction()
+        transaction.replace(R.id.rightLayout,fragment)
+        transaction.commit()
+    }
+}
